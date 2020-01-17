5fd21f2a4947c7b96ea0fb9e5f4fa969f57e2078
Guillermo González <ggonzalezh15@gmail.com>
2020-01-17 18:52:31 -0300

    arrowFunctions and npm start

--- src/firebase/getTitle.js
+++ src/firebase/getTitle.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-exports.getTitle = function() {
+exports.getTitle = () => {
     return new Promise((resolve, reject) => {
-        request(process.env.TITLE_ENDPOINT, (err, res, body) => {
+        request(/*process.env.TITLE_ENDPOINT*/'https://firestore.googleapis.com/v1/projects/dayz-las/databases/(default)/documents/discord-bot/title/', (err, res, body) => {
             body = JSON.parse(body)
             if(!err && body){
                 resolve(body.fields)
